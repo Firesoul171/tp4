2bf105eff7a4506e34d74e1796037740c46bc5a3
Sebastien Gendron <sebastien-24@hotmail.com>
2021-05-19 15:07:06 -0400

    modification  de secure.zone.php
    utilise le template html importer pour rentre secure.zone.php moins laid
    
    et ajouter 2 boutons de redirection vers allVisit.php et addVisit.php respectivement

--- html/secure.zone.php
+++ html/secure.zone.php
@@ -1,29 +1,66 @@
 <?php 
-/**
- * La configuration et le démarrage des sessions doit être fait avant le début 
- * de l'envoi de la réponse.
- */
 
-require_once 'auth.session.succesful.php';
+require_once '../session/auth.session.succesful.php';
     
     if (getSessionExiste()){
         $nom = $_SESSION['AuthInformation'];
 
     } else {
         setcookie("AuthError", "Vous devez vous authentifier", 0, "/", "gendron.techinfo420.ca", true, true);
-        header("Location: auth.ask.php");      
+        header("Location: ../session/auth.ask.php");      
     }
 
 ?>
-<!DOCTYPE html>
+<!DOCTYPE HTML>
+<!--
+	Stellar by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        echo "Ici c'est la zone protégée pour ".$nom;
-        ?>
-    </body>
+	<head>
+		<title>Acceuil</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="style.css">
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	
+	<body class="is-preload">
+	<?php ?>
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<h1>Application de comptabilisation de présences dans des lieux publics</h1>
+					</header>
+				<!-- Main -->
+					<div id="main">
+						<!-- Content -->
+							<section id="content" class="main">
+										<div>
+											<div>
+												<ul class="actions">
+													<li class="liste"><button id ="allVisit" href="#" class="button">Voir mes déplacements et changer mon état de santé durant ces déplacements</button></li>
+													<li class="liste"><button id ="addVisit" href="#" class="button">Ajouter un déplacement</button></li>
+												</ul>
+											</div>
+										</div>
+									</section>
+																		
+
+		<!-- Scripts -->
+		
+			<script src="formulaire.js"></script>
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+	</form>
+	</body>
 </html>

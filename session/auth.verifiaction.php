1feb46fe718752476dc9f570ed39289a8deffb11
Sebastien Gendron <sebastien-24@hotmail.com>
2021-05-19 15:08:41 -0400

    Modification des liens pour les require_once
    changer les liens des require_once pour prendre en compte la structure de fichier implenter et enlever les error_log de debugage.

--- session/auth.verifiaction.php
+++ session/auth.verifiaction.php
@@ -2,23 +2,22 @@
 
 if (!isset($_POST['name'],$_POST['password'])){
     setcookie("AuthFailed", "Refus de traitement.", 0, "/", "gendron.techinfo420.ca", true, true);
-    header("Location: auth.ask.php");
+    header("Location: ../session/auth.ask.php");
     
 } else if (empty($_POST['name']) || empty($_POST['password'])){
 
     setcookie("AuthFailed", "veuillez entrer tout les champs s’il vous plaît", 0, "/", "gendron.techinfo420.ca", true, true);
-    header("Location: auth.ask.php");
+    header("Location: ../session/auth.ask.php");
     
 } else {
 
-    require_once 'authentification.login.php';
+    require_once './authentification.login.php';
 
     $name = filter_input(INPUT_POST,'name',FILTER_SANITIZE_SPECIAL_CHARS);
     $password = filter_input(INPUT_POST,'password',FILTER_DEFAULT);
 
 
     if (AuthentificationLogin::getAuthentification($name,$password)){
-        error_log("Hello from the other side");
         require_once 'auth.session.succesful.php';
         setSession($name);
         header("Location: ../html/secure.zone.php");
